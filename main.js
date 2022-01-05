da535206f72f5b32d1f40adb1825f1ee43b33e3c
tapishnudey <78951151+tapishnudey@users.noreply.github.com>
2022-01-05 16:27:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function uploadimg() {
 	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
-window.addEventListener(keydown, my_keydown);
+window.addEventListener(keydown, 'my_keydown');
 
 function my_keydown(e)
 {
